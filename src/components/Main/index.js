3049a555876b05730625a2612dbc45628e4f040a
Fliss317 <felicity.cullen@mail.com>
2022-11-10 16:58:51 +0000

    Tasks 1-4 complete, task 5 in progress

--- src/components/Main/index.js
+++ src/components/Main/index.js
@@ -6,10 +6,20 @@ import samplePosts from '../../libs/data';
 
 import Post from '../Post';
 
+// use .map to loop through posts 
+// each item within array should return component 
+
+
 function Main() {
   const [posts, setPosts] = useState(samplePosts);
+  return <main id="main">{posts.map((posty) => {return <Post key={posty.postId} title={posty.title} date={posty.date} author={posty.author} text={posty.text} highlights={posty.highlights} image={posty.image}/>})}</main>
 
-  return <main id="main"></main>;
+
+  // return <main id="main">{posts.map((posty) => {return <Post key={posty.postId} title = {props.title} date ={props.date} author = {props.author} text = {props.text} highlights = {props.highlights} image = {props.image}/>})}</main>;
 }
 
 export default Main;
+
+// const listPosts = posts.map((posts) =>  <div text=
+//return <main id="main">{posts.map((posts) => <Post key={posts.postId} />)}</main>
+// title, date, author, text, highlights, image
--- src/components/Post/index.js
+++ src/components/Post/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 
 import './index.css';
 
-function Post() {
-  return <article></article>;
+function Post(props) {
+  console.log(props)
+  return <article>
+    <h1>{props.title}</h1>
+    <h2>{props.author}</h2>
+    <text>{props.text}</text>
+    <ul>{props.highlights}</ul>
+    <img src={props.image.link}/>
+  </article>;
 }
 
 export default Post;
+//title, date, author, text, highlights, image
